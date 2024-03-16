fcc03fe650135aa913636b7e0548d0a67feeac26
sikakwesi2020 <67636524+sikakwesi2020@users.noreply.github.com>
2024-03-16 09:11:37 -0400

    Initial Commit

---
+++ PennHack/PennHackApp.swift
@@ -0,0 +1,17 @@
+//
+//  PennHackApp.swift
+//  PennHack
+//
+//  Created by MAXWELL TAWIAH on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct PennHackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
